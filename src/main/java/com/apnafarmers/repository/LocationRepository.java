4d43a6dd89ec2905152185c519812adb3a2718a1
vivesing <vivek.singh1693@gmail.com>
2023-03-10 23:33:34 +0530

    [vivek] adding changes location repository

---
+++ src/main/java/com/apnafarmers/repository/LocationRepository.java
@@ -0,0 +1,15 @@
+package com.apnafarmers.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apnafarmers.entity.Location;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+	List<Location> findLocationByPinCode(long pinCode);
+
+}
